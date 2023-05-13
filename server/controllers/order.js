633231378b19830efd9cd660ff0edb3ade77ea96
urielda00 <urielda00@gmail.com>
2023-05-13 20:08:41 +0300

    added the read orders functionality to the order.js controller

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -3,27 +3,27 @@ import moment from 'moment/moment.js';
 const currentDate = moment();
 
 
-
 //The schema's that involve while creating an order:
 import Address from "../models/Address.js";
 import Invoice from "../models/Invoice.js";
 import Product from "../models/Products.js";
+import User from "../models/User.js";
 
-//create variable that represent the user:userId.
+ 
+
+//later, need to add: 1) totalprice backend calc. 
+//2) check if the quantity is not too much for what we have in the stock!
+//both need a loop.
 
 
 //Create
 export const createOrder = async(req,res)=>{
-
-  
   try {
 
   const id = req.params.id;
   const {address, productsId, totalPrice} = req.body; //productsId should be an array of objects!!
   const dateis = currentDate.format('MM-DD-YYYY hh:mm A');
   const date = new Date(dateis)
-  
-  
   //Create new order:
   const saveOrder= new Order({
     address,
@@ -36,7 +36,7 @@ export const createOrder = async(req,res)=>{
   
   
   //Update Address-
-  const searchAddressByUserId= await Address.findOne({userId:id}); //check if there is address relative to this user.
+  const searchAddressByUserId= await Address.findOne({userId:id}); 
   if(searchAddressByUserId){
     //if there is, update the address:
     const updateAddress = await Address.findOneAndUpdate({userId:id},
@@ -53,8 +53,8 @@ export const createOrder = async(req,res)=>{
   }  
 
   //Update Invoices:
-  const searchInvoiceByUserId= await Invoice.findOne({userId:id}); //check if there is invoice relative to this user.
-  const order= await Order.findOne({date, userId:id}); //check this syntex!!!!!
+  const searchInvoiceByUserId= await Invoice.findOne({userId:id}); 
+  const order= await Order.findOne({date, userId:id});
   const orderId= order.id;
   if(searchInvoiceByUserId){
     const updateInvoice= await Invoice.findOneAndUpdate({userId:id},
@@ -85,21 +85,29 @@ export const createOrder = async(req,res)=>{
     let result= product.quantity-quantity;
     const updatedProduct= await Product.findByIdAndUpdate(productId.id,{quantity:result})
   }   
-  res.status(200).send("Your order has been successfully placed and the new address created and also invoice and products!");
-
+  res.status(200)
+  .send("Your order has been successfully placed and the new address created and also invoice and products!");
   } catch (error) {
-    res.status(500).json(error.message)
+    res.status(500).json(error.message);
   }
-}
+};
 
 
 
-//Read 
+//Read Orders Relative To A Specific User.
+export const readOrders = async(req,res)=>{
+  try {
+  const id= req.params.id;
+  const orders= await Order.find({userId:id});
+  res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error.message);
+  } 
+};
 
 
 
-
-//Update
+//Update -this is might not be necessasry..]mj
 
 
 
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,11 +1,11 @@
 import express from "express";
 const orderRouter = express.Router();
-import { createOrder } from "../controllers/order.js";
+import { createOrder, readOrders } from "../controllers/order.js";
 
 
 
 orderRouter.post('/createOrder/:id', createOrder);
-
+orderRouter.get('/readOrders/:id',readOrders);
 
 
 export default orderRouter;
