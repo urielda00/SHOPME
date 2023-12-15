df8dd2261c4e2324f635c65c852a93aecaee66a2
urielda00 <urielda00@gmail.com>
2023-12-15 12:30:01 +0200

    Fix cors error

--- server/index.js
+++ server/index.js
@@ -20,28 +20,27 @@ const port = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
 
-
 //Middlewares:
-app.use(helmet());
-app.disable('etag');
-app.use(fileupload());
 app.use(express.json());
-app.use(cookieParser());
-app.use(morgan('common'));
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/searchProduct', express.static('uploads'));
-app.use('/product/readProducts', express.static('uploads'));
 app.use(express.urlencoded({ limit: '30mb', extended: false }));
-// app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' })); //for XXS protection.
-app. use( helmet({ crossOriginOpenerPolicy: false, }) );
+app.use(cors());
+app.use(morgan('common')); //the common loger of morgan
+app.use(helmet());
+app.use(cookieParser());
+app.use(fileupload());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' })); //for XXS protection.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/product/readProducts', express.static('uploads'));
+app.use('/searchProduct', express.static('uploads'));
+app.use(cookieParser());
+app.disable('etag');
 
 //Routers:
-app.use('/cart', cartRouter);
-app.use('/order', orderRouter);
 app.use('/auth', userAuthRouter);
 app.use('/product', productRouter);
+app.use('/order', orderRouter);
 app.use('/resetPass', resetPassRouter);
+app.use('/cart', cartRouter);
 
 //Connect to the DB and then listen at the port:
 mongoose
